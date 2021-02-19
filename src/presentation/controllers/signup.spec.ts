6d110872c3ffc11b8e116b114aa726bbcdb4f03f
peixe <guilherme.o.peixe@gmail.com>
2021-02-19 12:21:03 -0300

    test: ensure SignUpController returns 400 if password confirmation is diff

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -143,3 +143,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new ServerError())
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should returns 400 if password confirmation fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'diff_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+})
