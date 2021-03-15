4374c076bc45f16d6e8306dbf49ae76c5e57db7c
peixe <guilherme.o.peixe@gmail.com>
2021-03-15 16:27:18 -0300

    refactor: update authentication protocols

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -1,4 +1,4 @@
-export * from '../../../domain/usecases/authentication'
+export * from '../../../domain/useCases/authentication'
 export * from '../../protocols/db/account/load-account-by-email-repository'
 export * from '../add-account/db-add-account-protocols'
 export * from '../../protocols/criptography/hash-comparer'
