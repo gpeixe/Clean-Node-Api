287f9454ee229d53334935c88e5dfc67a5958713
peixe <guilherme.o.peixe@gmail.com>
2021-03-15 15:41:23 -0300

    feat: ensure AddSurveyController returns 400 if Validation returns an error

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,16 @@
+import { badRequest } from '../../../helpers/http/http-helper'
+import { Controller, HttpRequest, HttpResponse, Validation } from '../../../protocols'
+
+export class AddSurveyController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest)
+    if (error) {
+      return badRequest(error)
+    }
+    return await new Promise(resolve => resolve(null))
+  }
+}
