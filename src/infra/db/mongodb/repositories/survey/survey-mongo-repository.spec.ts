8a0c60df2d4e9ba1be7b6e81b85d4c8aa88a7b7f
peixe <guilherme.o.peixe@gmail.com>
2021-03-15 17:29:05 -0300

    test: ensure SurveyMongoRepository create a survey on add success

---
+++ src/infra/db/mongodb/repositories/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,45 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+import { AddSurveyModel } from '../../../../../data/usecases/add-survey/db-add-survey-protocols'
+
+let surveyCollection: Collection
+
+const makeFakeAddSurvey = (): AddSurveyModel => {
+  return {
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'other_answer'
+    }]
+  }
+}
+
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
+describe('Survey MongoDb Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should create an survey on add success', async () => {
+    const sut = makeSut()
+    const addSurvey = makeFakeAddSurvey()
+    await sut.add(addSurvey)
+    const survey = await surveyCollection.findOne({ question: addSurvey.question })
+    expect(survey).toBeTruthy()
+  })
+})
