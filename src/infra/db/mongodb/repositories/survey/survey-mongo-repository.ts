9cd9ced98270a02a4fc4a08a2ce1e47e6fddc232
peixe <guilherme.o.peixe@gmail.com>
2021-03-15 17:28:28 -0300

    feat: ensure SurveyMongoRepository create a survey on add success

---
+++ src/infra/db/mongodb/repositories/survey/survey-mongo-repository.ts
@@ -0,0 +1,16 @@
+import { Collection } from 'mongodb'
+import { AddSurveyModel, AddSurveyRepository } from '../../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { MongoHelper } from '../../helpers/mongo-helper'
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  private async getSurveyCollection (): Promise<Collection> {
+    return await MongoHelper.getCollection('surveys')
+  }
+
+  async add (surveyData: AddSurveyModel): Promise<void> {
+    const surveyCollection = await this.getSurveyCollection()
+    const result = await surveyCollection.insertOne(surveyData)
+    const survey = result.ops[0]
+    return MongoHelper.map(survey)
+  }
+}
