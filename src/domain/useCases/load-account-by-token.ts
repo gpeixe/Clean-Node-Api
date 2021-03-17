91cf6f1a40c9c1940c13ca45486a0191a749e23d
peixe <guilherme.o.peixe@gmail.com>
2021-03-17 13:53:57 -0300

    feat: ensure DbLoadAccountByToken calls Decrypter with correct value

--- src/domain/useCases/load-account-by-token.ts
+++ src/domain/useCases/load-account-by-token.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../models/account'
 
 export interface LoadAccountByToken {
-  loadByToken: (token: string, role?: string) => Promise<AccountModel>
+  load: (token: string, role?: string) => Promise<AccountModel>
 }
