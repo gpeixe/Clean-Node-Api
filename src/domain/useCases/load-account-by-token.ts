ce3ffed93e89d1720fcc91dd7bb1d8d7bb5654a0
peixe <guilherme.o.peixe@gmail.com>
2021-03-17 13:07:21 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct role

--- src/domain/useCases/load-account-by-token.ts
+++ src/domain/useCases/load-account-by-token.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../models/account'
 
 export interface LoadAccountByToken {
-  loadByToken: (token: string) => Promise<AccountModel>
+  loadByToken: (token: string, role?: string) => Promise<AccountModel>
 }
