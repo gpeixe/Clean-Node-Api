e4762ee3aaf21e7a51f44eeef2dd85aaa0291894
Guilherme Peixe <guilherme@softable.com.br>
2021-03-29 21:21:30 -0300

    refator: change MongoHelper name

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -1,6 +1,6 @@
 import { MongoHelper as sut } from './mongo-helper'
 import env from '../../../../main/config/env'
-describe('Mongo Helper', () => {
+describe('MongoHelper', () => {
   beforeAll(async () => {
     console.log(process.env.MONGO_URL)
     await sut.connect(env.mongoUrl)
