99e64b1c80d8d35b9eeb9c9e93fa3c21798ed4fd
peixe <guilherme.o.peixe@gmail.com>
2021-06-06 02:49:50 -0300

    refactor: move auth middlewares to separate files

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-factory.ts
@@ -0,0 +1,9 @@
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys.factory'
+import { LoadSurveysController } from '@/presentation/controllers/survey/load-survey/load-surveys-controller'
+import { Controller } from '@/presentation/protocols'
+
+export const makeLoadSurveysController = (): Controller => {
+  const loadSurveysController = new LoadSurveysController(makeDbLoadSurveys())
+  return makeLogControllerDecorator(loadSurveysController)
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeAddSurveyController()))
 }
